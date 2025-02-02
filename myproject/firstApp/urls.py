9b9c64001822b095aa646af506b95a68790706b9
codder228 <codder199912@gmail.com>
2025-02-02 14:54:10 +0300

    Start

--- myproject/firstApp/urls.py
+++ myproject/firstApp/urls.py
@@ -1,7 +1,4 @@
 from django.urls import path
 from firstApp import views
 
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("about/", views.about, name="about"),
-]
+urlpatterns = []
--- myproject/firstApp/views.py
+++ myproject/firstApp/views.py
@@ -1,12 +1,1 @@
 from django.shortcuts import render
-
-
-# Create your views here.
-def index(request):
-    context = {"title": "RateYourGames - Главная", "content": "Главная страница сайта"}
-    return render(request, "index.html", context=context)
-
-
-def about(request):
-    context = {"title": "О нас", "content": "О нас"}
-    return render(request, "about.html", context=context)
